99c85f4a7cc89350aa1c056519432e01b81ae8d5
Luis Cesar Albuquerque Arruda <lc.albuquerque@edu.unifor.br>
2023-09-14 18:03:03 -0300

    header do front-end

--- upload-ia-web/src/App.tsx
+++ upload-ia-web/src/App.tsx
@@ -1,10 +1,31 @@
-
-
+import React from "react";
+import { Github } from "lucide-react";
+import { Button } from "./components/ui/button";
+import { Separator } from "@radix-ui/react-separator";
 export function App() {
 
-  return (
-    <h1>Hello word</h1>
-  )
+	return (
+		<div>
+			<div className="px-6 py-3 flex items-center justify-between border-b-0">
+
+				<h1 className="text-xl font-bold"> Upload.ai</h1>
+
+				<div className="flex items-center gap-3">
+
+					<span className="text-sm text-muted-foreground">
+					Desenvolvido com ❤️ no NLW da Rocketseat
+					</span>
+					
+					<Separator orientation="vertical" className=" h-6"/>
+
+					<Button>
+						<Github className ="w-4 h-4 mr-2"/>
+            GitHub
+					</Button>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 
